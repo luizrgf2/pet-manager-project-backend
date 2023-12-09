88177d4e30a1dba4cdb8658cc30b4a8dbe3c8a9a
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-09 09:57:31 -0300

    📦 FEAT: add error base to error handling

---
+++ internal/core/errors/base_error.go
@@ -0,0 +1,12 @@
+package errors
+
+import "fmt"
+
+type ErroBase struct {
+	Message string
+	Code    uint
+}
+
+func (e *ErroBase) Error() string {
+	return fmt.Sprintf("Error code %d : %s", e.Code, e.Message)
+}
