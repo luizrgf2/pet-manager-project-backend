19e2ebb84373411a3fac4b68e3c5175de3d89473
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-09 09:50:12 -0300

    📦 FEAT: add new erros to user

---
+++ internal/core/errors/user.go
@@ -0,0 +1,19 @@
+package errors
+
+const (
+	UserPasswordLenErrorCode         = 422
+	UserPasswordUpperLetterErrorCode = 422
+	UserEmailInvalidErrorCode        = 422
+	UserNameInvalidErrorCode         = 422
+	UserNotExistsErrorCode           = 404
+	UserAlreadyExistsErrorCode       = 409
+)
+
+const (
+	UserPasswordLenErrorMessage         = "A senha deve ter entre 8 e 15 caracteres!"
+	UserPasswordUpperLetterErrorMessage = "A senha deve ter pelo menos uma letra maiúscula!"
+	UserEmailInvalidErrorMessage        = "O email é inválido!"
+	UserNameInvalidErrorMessage         = "O nome deve ter de 4 a 50 caracteres!"
+	UserNotExistsErrorMessage           = "O usuário não existe!"
+	UserAlreadyExistsErrorMessage       = "O usuário já existe!"
+)
