9e78cdf606b290743fa61c4353a248e7b8ca18e8
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-09 22:01:17 -0300

    📦 FEAT: new tests to createUserUseCase

---
+++ internal/data/usecase/user/create_user_test.go
@@ -0,0 +1,83 @@
+package usecases_test
+
+import (
+	"testing"
+
+	errors_core "github.com/luizrgf2/pet-manager-project-backend/internal/core/errors"
+	usecases_interfaces "github.com/luizrgf2/pet-manager-project-backend/internal/core/usecase/user"
+	errors_data "github.com/luizrgf2/pet-manager-project-backend/internal/data/error"
+	usecases_imp "github.com/luizrgf2/pet-manager-project-backend/internal/data/usecase/user"
+	tests_moks "github.com/luizrgf2/pet-manager-project-backend/test/moks/service"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateUserUseCase(t *testing.T) {
+
+	cepService := tests_moks.CEPServiceInMemory{}
+
+	sut := usecases_imp.CreateUserUseCase{CepService: cepService}
+
+	userToTeste := usecases_interfaces.InputCreateUserUseCase{
+		NamePet:        "Felicidog pet salon",
+		Email:          "email@valid.com",
+		Password:       "Teste12345",
+		AddrCep:        "38705280",
+		AddrComplement: "",
+		AddrNumber:     622,
+	}
+
+	_, err := sut.Exec(userToTeste)
+
+	assert.Nil(t, err)
+
+}
+
+func TestReturnErrorWIthInvalidEmail(t *testing.T) {
+
+	cepService := tests_moks.CEPServiceInMemory{}
+	sut := usecases_imp.CreateUserUseCase{CepService: cepService}
+
+	expectedError := errors_core.ErroBase{
+		Message: errors_core.UserEmailInvalidErrorMessage,
+		Code:    errors_core.UserNameInvalidErrorCode,
+	}
+
+	userToTeste := usecases_interfaces.InputCreateUserUseCase{
+		NamePet:        "Felicidog pet salon",
+		Email:          "email@valid",
+		Password:       "Teste12345",
+		AddrCep:        "38705280",
+		AddrComplement: "",
+		AddrNumber:     622,
+	}
+
+	_, err := sut.Exec(userToTeste)
+
+	assert.Equal(t, expectedError.Error(), err.Error())
+
+}
+
+func TestReturnErrorWIthInvalidCep(t *testing.T) {
+
+	cepService := tests_moks.CEPServiceInMemory{}
+	sut := usecases_imp.CreateUserUseCase{CepService: cepService}
+
+	expectedError := errors_core.ErroBase{
+		Message: errors_data.CEPInvalidErrorMessage,
+		Code:    uint(errors_data.CEPInvalidErrorCode),
+	}
+
+	userToTeste := usecases_interfaces.InputCreateUserUseCase{
+		NamePet:        "Felicidog pet salon",
+		Email:          "email@valid.com",
+		Password:       "Teste12345",
+		AddrCep:        "234",
+		AddrComplement: "",
+		AddrNumber:     622,
+	}
+
+	_, err := sut.Exec(userToTeste)
+
+	assert.Equal(t, expectedError.Error(), err.Error())
+
+}
