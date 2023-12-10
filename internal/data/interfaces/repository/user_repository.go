88a3a0e824494a4b414db42308a2f620aabce033
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-10 09:22:35 -0300

    📦 FEAT: add

--- internal/data/interfaces/repository/user_repository.go
+++ internal/data/interfaces/repository/user_repository.go
@@ -1,4 +1,17 @@
 package repository
 
+type CreateUserRepositoryInput struct {
+	NamePet        string
+	Email          string
+	Password       string
+	AddrStreet     string
+	AddrNumber     uint
+	AddrComplement *string
+	AddrDistrict   string
+	AddrCity       string
+	AddrState      string
+}
+
 type UserRepositoryInterface interface {
+	create()
 }
