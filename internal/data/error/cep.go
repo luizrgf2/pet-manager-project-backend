e20f391c4a90106966d094b79184ac9fe34e59e7
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-09 22:12:38 -0300

    📦 FEAT: create new erros to cep

---
+++ internal/data/error/cep.go
@@ -0,0 +1,11 @@
+package error
+
+var (
+	CEPInvalidErrorCode   = 422
+	CEPNotExistsErrorCode = 404
+)
+
+var (
+	CEPInvalidErrorMessage   = "O cep informado não é válido!"
+	CEPNotExistsErrorMessage = "O cep informado não existe!"
+)
