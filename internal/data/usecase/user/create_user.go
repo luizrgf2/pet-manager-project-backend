c754f8cfedb11c5d73cb562e1e97ec42aed6b289
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-09 22:00:57 -0300

    📦 FEAT: add new validation to cep in usecase

--- internal/data/usecase/user/create_user.go
+++ internal/data/usecase/user/create_user.go
@@ -1,24 +1,47 @@
 package usecases
 
 import (
+	"regexp"
+
 	"github.com/luizrgf2/pet-manager-project-backend/internal/core/entity"
+	core_errors "github.com/luizrgf2/pet-manager-project-backend/internal/core/errors"
 	usecases "github.com/luizrgf2/pet-manager-project-backend/internal/core/usecase/user"
-	"github.com/luizrgf2/pet-manager-project-backend/internal/data/interfaces/services"
+	data_errors "github.com/luizrgf2/pet-manager-project-backend/internal/data/error"
+	services "github.com/luizrgf2/pet-manager-project-backend/internal/data/interfaces/service"
 )
 
 type CreateUserUseCase struct {
-	cepService services.CEPServiceInterface
+	CepService services.CEPServiceInterface
 }
 
 func (c *CreateUserUseCase) getAddrWithCep(cep string) (*services.AddrProps, error) {
-	addr, err := c.cepService.GetAddr(cep)
+	addr, err := c.CepService.GetAddr(cep)
 	if err != nil {
 		return nil, err
 	}
-	return &addr, nil
+	return addr, nil
+}
+
+func (c *CreateUserUseCase) validateCep(cep string) error {
+	regexCEP := regexp.MustCompile(`^\d{8}$`)
+	isValid := regexCEP.MatchString(cep)
+
+	if !isValid {
+		return &core_errors.ErroBase{
+			Message: data_errors.CEPInvalidErrorMessage,
+			Code:    uint(data_errors.CEPInvalidErrorCode),
+		}
+	}
+	return nil
 }
 
 func (c *CreateUserUseCase) Exec(input usecases.InputCreateUserUseCase) (*usecases.OutputCreateuserUseCase, error) {
+
+	err := c.validateCep(input.AddrCep)
+
+	if err != nil {
+		return nil, err
+	}
 
 	addr, err := c.getAddrWithCep(input.AddrCep)
 
