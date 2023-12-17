ec293904eec8b4015614b09872d7a05b38643aba
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-17 11:28:41 -0300

    📦 FEAT: add new script to test integrations tests for user entity

---
+++ scripts/run_tests_integration_users.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Nome do contêiner e banco de dados
+CONTAINER_NAME="db_test"
+CONTAINER_DB_USER="test"
+CONTAINER_DB_PASSWORD="test"
+CONTAINER_DB_NAME="petManager"
+
+export ENVIROMENT=true
+
+# Verificar se o contêiner já está em execução
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    echo "O contêiner ${CONTAINER_NAME} já está em execução."
+else
+    # Se o contêiner não estiver em execução, iniciar
+    docker run -e MYSQL_USER=${CONTAINER_DB_USER} -e MYSQL_PASSWORD=${CONTAINER_DB_PASSWORD} -e MYSQL_DATABASE=${CONTAINER_DB_NAME} -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d --name=${CONTAINER_NAME} mysql
+
+    # Aguardar alguns segundos para garantir que o MySQL tenha tempo de iniciar
+    sleep 60
+fi
+
+migrate -path=db/migrations -database="mysql://${CONTAINER_DB_USER}:${CONTAINER_DB_PASSWORD}@tcp(localhost:3306)/petManager" up
+
+# Executar seus testes aqui
+go test test/integrations/services/user/*
+
+# Parar e remover o contêiner após a execução dos testes
+docker stop ${CONTAINER_NAME}
+docker rm ${CONTAINER_NAME}
