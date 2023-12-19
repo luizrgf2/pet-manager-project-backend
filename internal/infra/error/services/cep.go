48a9a304912efee13ba7bd68663cf257bec05512
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-18 23:03:01 -0300

    📦 FEAT: create erros to cep service

---
+++ internal/infra/error/services/cep.go
@@ -0,0 +1,11 @@
+package error
+
+var (
+	CEPServiceUnavailableErrorMessage = "O serviço de cep não está disponível!"
+	CEPInvalidErrorMessage            = "O cep informado é inválido!"
+)
+
+var (
+	CEPServiceUnavailableErrorCode = uint(503)
+	CEPInvalidErrorCode            = uint(400)
+)
