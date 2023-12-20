e1f49870c8d9ff857b9bb2aa55b505f0f1e7beb4
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-20 09:04:52 -0300

    📦 FEAT: add new erros to jwt

---
+++ internal/infra/error/services/jwt.go
@@ -0,0 +1,11 @@
+package error
+
+var (
+	JWTIvalidTokenErrorMessage        = "O token fornecido não é válido!"
+	JWTErrorToCreateTokenErrorMessage = "Aconteceu algum erro na criação do token!"
+)
+
+var (
+	JWTIvalidTokenErrorCode        = uint(401)
+	JWTErrorToCreateTokenErrorCode = 500
+)
