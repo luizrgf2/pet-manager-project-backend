b1ae63fb72362386655e60625b91ca857366c3ff
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-22 18:06:46 -0300

    📦 FEAT: create server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/luizrgf2/pet-manager-project-backend/config"
+	routes "github.com/luizrgf2/pet-manager-project-backend/internal/main"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	err := http.ListenAndServe(fmt.Sprintf(":%d", config.API_PORT), routes.Router)
 	if err != nil {
-		panic("Error to load enviroment variables")
+		log.Fatal("Erro para iniciar o server", err.Error())
 	}
+	fmt.Println("Sucess sever is running")
 }
