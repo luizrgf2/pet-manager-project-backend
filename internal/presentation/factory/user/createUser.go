41153911fedbf44641772f7324d9a0f9daa8dc54
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-22 12:54:27 -0300

    📦 FEAT: add CreateUserFactoryController and CreateUserFactoryUseCase

---
+++ internal/presentation/factory/user/createUser.go
@@ -0,0 +1,37 @@
+package factories
+
+import (
+	usecases_interfaces "github.com/luizrgf2/pet-manager-project-backend/internal/core/usecase/user"
+	usecases "github.com/luizrgf2/pet-manager-project-backend/internal/data/usecase/user"
+	repository "github.com/luizrgf2/pet-manager-project-backend/internal/infra/repository"
+	"github.com/luizrgf2/pet-manager-project-backend/internal/infra/service"
+	controller "github.com/luizrgf2/pet-manager-project-backend/internal/presentation/controllers/user"
+)
+
+func CreateUserFactoryController() controller.CreateUserController {
+	service1 := service.CepServiceViaCep{}
+	service2 := service.HashService{}
+	repo := repository.UserRepository{}
+
+	usecase := usecases.CreateUserUseCase{
+		CepService:     service1,
+		UserRepository: repo,
+		HashService:    service2,
+	}
+	controller := controller.CreateUserController{Usecase: usecase}
+	return controller
+}
+
+func CreateUserFactoryUseCase() usecases_interfaces.CreateUserUseCaseInterface {
+	service1 := service.CepServiceViaCep{}
+	service2 := service.HashService{}
+	repo := repository.UserRepository{}
+
+	usecase := usecases.CreateUserUseCase{
+		CepService:     service1,
+		UserRepository: repo,
+		HashService:    service2,
+	}
+
+	return usecase
+}
