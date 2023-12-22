4381eb3cbedf2e93100a14e2255c368b555b6477
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-22 11:00:56 -0300

    🛠  REFACTOR: change error handling

--- internal/presentation/controllers/user/createUser.go
+++ internal/presentation/controllers/user/createUser.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
-	core_errors "github.com/luizrgf2/pet-manager-project-backend/internal/core/errors"
 	usecases_interfaces "github.com/luizrgf2/pet-manager-project-backend/internal/core/usecase/user"
+	"github.com/luizrgf2/pet-manager-project-backend/internal/presentation/common"
 	"github.com/luizrgf2/pet-manager-project-backend/internal/presentation/contracts"
 )
 
@@ -32,35 +32,13 @@ func (c *CreateUserController) validateFields(input InputCreateUserController) [
 	return errorsToReturn
 }
 
-func (c *CreateUserController) handleErrors(err error) contracts.HTTPResponse[usecases_interfaces.OutputCreateuserUseCase] {
-	if err, ok := err.(*core_errors.ErroBase); ok && err != nil {
-		return contracts.HTTPResponse[usecases_interfaces.OutputCreateuserUseCase]{
-			Response:      nil,
-			ErrorsMessage: []string{err.Message},
-			Code:          err.Code,
-		}
-	}
-
-	return contracts.HTTPResponse[usecases_interfaces.OutputCreateuserUseCase]{
-		Response:      nil,
-		ErrorsMessage: []string{err.Error()},
-		Code:          500,
-	}
-}
-
-func (c *CreateUserController) handleFieldsErrors(messageErrors []string) contracts.HTTPResponse[usecases_interfaces.OutputCreateuserUseCase] {
-	return contracts.HTTPResponse[usecases_interfaces.OutputCreateuserUseCase]{
-		Response:      nil,
-		ErrorsMessage: messageErrors,
-		Code:          400,
-	}
-}
-
 func (c *CreateUserController) Handle(input InputCreateUserController) contracts.HTTPResponse[usecases_interfaces.OutputCreateuserUseCase] {
+
+	errorHandling := common.ErrorHandling[usecases_interfaces.OutputCreateuserUseCase]{}
 
 	errorsToValidateFields := c.validateFields(input)
 	if len(errorsToValidateFields) > 0 {
-		return c.handleFieldsErrors(errorsToValidateFields)
+		return errorHandling.HandlingFieldicError(errorsToValidateFields)
 	}
 
 	input_usecase := usecases_interfaces.InputCreateUserUseCase{
@@ -75,7 +53,7 @@ func (c *CreateUserController) Handle(input InputCreateUserController) contracts
 	res, err := c.usecase.Exec(input_usecase)
 
 	if err != nil {
-		return c.handleErrors(err)
+		return errorHandling.HandlingGenericError(err)
 	}
 
 	outputRes := usecases_interfaces.OutputCreateuserUseCase{
