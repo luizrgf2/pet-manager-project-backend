dde1e92f0a0aedfbd8db8a6a416db8b8602d3231
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-12-24 12:10:31 -0300

    🛠  REFACTOR: change input

--- internal/main/controller/user/send_confirmation_email_to_user.go
+++ internal/main/controller/user/send_confirmation_email_to_user.go
@@ -3,26 +3,26 @@ package controller_user
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	controller_error "github.com/luizrgf2/pet-manager-project-backend/internal/main/error"
 	controller_interfaces "github.com/luizrgf2/pet-manager-project-backend/internal/presentation/controllers/user"
 	factories "github.com/luizrgf2/pet-manager-project-backend/internal/presentation/factory/user"
 )
 
 func SendConfirmationEmailToUserHttpController(w http.ResponseWriter, r *http.Request) {
-	input := controller_interfaces.InputSendEmailConfirmationToUserController{}
-
-	errJson := json.NewDecoder(r.Body).Decode(&input)
-
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	w.Header().Set("Content-Type", "application/json")
 
-	if errJson != nil {
-		fieldError := controller_error.InputFieldsErrorHTTP()
-		w.WriteHeader(int(fieldError.Code))
-		responseJson, _ := json.Marshal(&fieldError)
-		w.Write(responseJson)
-		return
+	if err != nil || id < 1 {
+		errorId := controller_error.InputIdInvalid()
+		resJson, _ := json.Marshal(errorId)
+		w.WriteHeader(int(errorId.Code))
+		w.Write(resJson)
 	}
+
+	input := controller_interfaces.InputSendEmailConfirmationToUserController{Id: uint(id)}
 
 	controller := factories.SendConfirmationEmailToUserFactoryController()
 	res := controller.Handle(input)
