53d0fed6110273473aef2801b0dd6e9da8d7d02e
Luiz Ricardo <luizrgfelipe@gmail.com>
2024-01-02 07:42:32 -0300

    ✅ TEST: create new method to sparete reponsability of creation of users in database

--- test/api/user/send_confirmation_email_to_user_test.go
+++ test/api/user/send_confirmation_email_to_user_test.go
@@ -3,44 +3,17 @@ package user_test
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/luizrgf2/pet-manager-project-backend/config"
-	usecases_interfaces "github.com/luizrgf2/pet-manager-project-backend/internal/core/usecase/user"
 	routes "github.com/luizrgf2/pet-manager-project-backend/internal/main"
 	controller "github.com/luizrgf2/pet-manager-project-backend/internal/presentation/controllers/user"
-	user_factories "github.com/luizrgf2/pet-manager-project-backend/internal/presentation/factory/user"
 	"github.com/stretchr/testify/assert"
 )
 
-func createUserToTest() {
-
-	userToTeste := usecases_interfaces.InputCreateUserUseCase{
-		NamePet:        "Felicidog pet salon",
-		Email:          config.SMTP_EMAIL_RECEIVER_TO_TEST,
-		Password:       "Teste12345",
-		AddrCep:        "38705280",
-		AddrComplement: "",
-		AddrNumber:     622,
-	}
-
-	createUserUseCase := user_factories.CreateUserFactoryUseCase()
-
-	_, err := createUserUseCase.Exec(userToTeste)
-
-	if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Println("user test 1 created!!")
-	}
-
-}
-
 func TestSendConfirmationEmail(t *testing.T) {
 
-	createUserToTest()
+	CreateUserToTest()
 
 	input := controller.InputSendEmailConfirmationToUserController{
 		Id: 1,
